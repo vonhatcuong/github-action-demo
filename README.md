10c0c4c849e6a9514179c5f722a4a5c4ca67303a
vonhatcuong <vonhatcuongyy@gmail.com>
2023-09-29 16:23:50 +0700

    create workflows

---
+++ README.md
@@ -0,0 +1,2 @@
+## This is my test CI/CD with github Action# github-action-demo
+# github-action-demo
---
+++ app/main_test.py
@@ -0,0 +1,17 @@
+from main import return_backwards_string, get_mode
+import unittest
+import os
+
+
+class TestMain (unittest.TestCase):
+    def test_return_backwards_string(sefl):
+        random_string = "this is my test string"
+        random_string_reversed = "gnirts tset ym si sihT"
+        self.assertEquals(random_string_reversed,return_backwards_string(random_string))
+
+    def test_get_env(self):
+        self.assertEquals(os.environ.get("MODE"),get_mode())
+
+
+if __name__ == "__main__":
+    unittest.main()
